c04d154b50221a51932ae9fe6a326c7d197b2909
zxhd <zxhd863943427@outlook.com>
2023-03-16 16:56:10 +0800

    完成自定义组件向父组件的状态传递

---
+++ src/components/Option.vue
@@ -0,0 +1,14 @@
+<template>
+    <n-switch  v-model:value="status" @update:value="$emit('update:open', status)"></n-switch>
+</template>
+
+<script lang="ts" setup>
+import { NSwitch } from 'naive-ui'
+import {ref} from 'vue'
+const props = defineProps({
+    open: Boolean
+})
+console.log(props.open)
+const status = ref(props.open)
+</script>
+
