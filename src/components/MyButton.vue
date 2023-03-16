55d47709e982902157022bff71d1c32950a20aed
zxhd <zxhd863943427@outlook.com>
2023-03-16 19:27:29 +0800

    完成按钮组件

---
+++ src/components/MyButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="option">
+        <div id="content">
+            <span style="float: left; margin: auto 0">{{ name }}</span>
+            <n-button @click="props.clickFunc" style="float: right" size="small"> warning </n-button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { NButton } from 'naive-ui'
+import { ref } from 'vue'
+const props = defineProps({
+    name: String,
+    clickFunc: Function
+})
+</script>
+
+<style scoped>
+#option {
+    display: grid;
+    align-items: center;
+    min-height: 40px;
+}
+
+#content {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
