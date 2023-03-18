1f5ed20467af860325fd6d3ae06afff07dd327ea
zxhd <zxhd863943427@outlook.com>
2023-03-18 17:40:23 +0800

    完成设置持久化

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,41 @@
-import { createApp,ref } from 'vue'
+import { createApp,ref,watch } from 'vue'
 import App from './App.vue'
 import TopButton from './components/TopButton.vue'
 import sheet from './components/sheet.vue'
 import { Plugin, Menu, clientApi } from 'siyuan'
 import { addCards,removeCards,openDynamiMarkCard } from './utils/card'
-
-const beautifulFeature = [
-  { content: "闪卡样式增强", status: ref(false) },
-  { content: "复习挖空增强", status: ref(false) },
-  { content: "类remnote复习界面", status: ref(false) },
-  { content: "层级闪卡", status: ref(false) },
-
-]
-
-const labFeature = [
-  { content: "内置卡包制卡", status: ref(true) },
-  { content: "数学块遮挡制卡", status: ref(false) },
-  { content: "沉浸式制卡", status: ref(false) },
-
-]
-const dangerousFeature = [
-  { content: "清除当页闪卡", func: removeCards },
-]
+import {settingList,getSetting} from "./utils/config"
 
 
-let settingConfig = {
-    beautifulFeature: beautifulFeature,
-    labFeature: labFeature,
-    dangerousFeature: dangerousFeature,
-  }
-
-
-
-// let app = createApp(App,settingConfig)
-// app.mount('#app')
 
 export default class CardPlugin extends Plugin {
     public el: HTMLElement
     public sheet: HTMLElement
+    public settingConfig : any
 
     constructor() {
         super()
         this.el = document.createElement('div')
         this.sheet = document.createElement('div')
+        this.settingConfig = null
     }
 
-    onload() {
-        
+    async onload() {
+        //加载本地配置
+        let localConfig = await this.loadStorage("enhanceConfig.json")
+        this.settingConfig = await getSetting(localConfig)
+        console.log("闪卡增强当前配置：\n",settingList.getSetting())
+
         const button = createApp(TopButton)
         button.mount(this.el)
         this.el.addEventListener('click', (event) => {
-            addCards(labFeature[0]["status"].value)
+            addCards(this.settingConfig.labFeature[0]["status"].value)
             event.stopPropagation()
             event.preventDefault()
         })
         this.el.addEventListener('contextmenu', (event) => {
             const menu = document.createElement('div')
-            const app = createApp(App,settingConfig)
+            const app = createApp(App,this.settingConfig)
             app.mount(menu)
             new Menu('CardPlugin').addItem({ element: menu }).showAtMouseEvent(event)
             event.stopPropagation()
@@ -69,20 +48,28 @@ export default class CardPlugin extends Plugin {
             shortcut: "alt+d",
             description: "根据菜单的选项，打开动态制卡功能",
             callback: () =>{openDynamiMarkCard(
-                labFeature[2]["status"].value,
-                labFeature[0]["status"].value)
+              this.settingConfig.labFeature[2]["status"].value,
+              this.settingConfig.labFeature[0]["status"].value)
                 },
     })
 
         //插入小型功能sheet
-        const AppSheet = createApp(sheet,settingConfig)
+        const AppSheet = createApp(sheet,this.settingConfig)
         AppSheet.mount(this.sheet)
         let base = document.querySelector("head")
         base?.appendChild(this.sheet)
+
+        watch(settingList.setList,()=>{this.writeConfig()})
     }
 
-    onunload() {
+    async onunload() {
         this.el && this.el.remove();
         this.sheet && this.sheet.remove();
+        this.writeConfig()
+    }
+
+    async writeConfig(){
+      let configText = JSON.stringify(settingList.getSetting())
+      await this.writeStorage("enhanceConfig.json",configText)
     }
 }
